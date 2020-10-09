b3e22defee61025241804b3d914af00d03546f26
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2020-10-09 21:12:35 +0700

    Day 2

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path')
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'public')))
+//Template Engime
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname, 'resoursces/views') )
+
+app.get('/', (req,res) => res.render('home'));
+app.get('/news', (req,res) => res.render('news'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
